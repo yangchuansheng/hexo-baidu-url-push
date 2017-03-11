c3d2b6d8d13fd8d6468af7e66b36a9938dc05c6a
XuanmiaoG <2291507486@qq.com>
2017-03-12 07:24:21 +0800

    build passing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 hexo.extend.filter.register('before_post_render', function(data){
-    var file_content = fs.readFileSync('./push.js');
+    var file_content = fs.readFileSync('./node_modules/hexo-baidu-url-push/push.js');
         data.content += file_content;
     return data;
 });
